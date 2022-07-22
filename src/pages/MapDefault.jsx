5b24f116abc2b74b7c3d9aff0ab74c6e5ae10637
Svitkin Arthur <svitkin.ae1604@gmail.com>
2022-07-22 14:25:58 +0300

    fix(MapDefault.jsx): починил лоадер

--- src/pages/MapDefault.jsx
+++ src/pages/MapDefault.jsx
@@ -1,14 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Map, Placemark, YMaps} from 'react-yandex-maps';
 import Loader from "../components/UI/loader/Loader";
 
 const MapDefault = () => {
     const [isMapLoading, setIsMapLoading] = useState(false);
-    useEffect(() => {
-        setTimeout(function () {
-            setIsMapLoading(true);
-        }, 2000);
-    });
 
     const mapState = {center: [56.7333, 37.1667], zoom: 12};
     const coordinate = [56.753429, 37.164175];
@@ -16,18 +11,15 @@ const MapDefault = () => {
     return (
         <div>
             <h1>Карта</h1>
-            {isMapLoading
-                ? <YMaps>
-                    <Map defaultState={mapState} width='100%' height='580px'>
-                        <Placemark geometry={coordinate}/>
-                    </Map>
-                </YMaps>
-                : <Loader/>
-            }
+            {!isMapLoading && <Loader/>}
+            <YMaps>
+                <Map defaultState={mapState} width='100%' height='580px' onLoad={() => setIsMapLoading(true)}>
+                    <Placemark geometry={coordinate}/>
+                </Map>
+            </YMaps>
             <br/>
         </div>
     );
-
 };
 
 export default MapDefault;
